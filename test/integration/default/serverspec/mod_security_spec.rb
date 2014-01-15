6d67f5260fe50d8b2cf662363c4b56e96bd85003
Gavin Reynolds <g.reynolds@src.gla.ac.uk>
2014-01-15 17:10:20 +0000

    Consistently naming the tests and added more in-depth tests.

--- test/integration/default/serverspec/mod_security_spec.rb
+++ test/integration/default/serverspec/mod_security_spec.rb
@@ -11,20 +11,30 @@ end
 
 describe "Apache2 with mod_security" do
 
-  it "is listening on port 80" do
-    expect(port(80)).to be_listening
+  it "apache2 should be installed" do
+    expect(package("apache2")).to be_installed
   end
 
-  it "has a running service of apache2" do
+  it "apache2 should be running" do
     expect(service("apache2")).to be_running
   end
 
-  it "has a mod-security configuration file" do
-    expect(file("/etc/apache2/mods-enabled/mod-security.conf")).to be_file
+  it "apache2 is listening on port 80" do
+    expect(port(80)).to be_listening
   end
 
-  it "has a mod-security load file" do
+  it "apache2 has a mod-security configuration file" do
+    expect(file("/etc/apache2/mods-enabled/mod-security.conf")).to be_file
+    expect(file("/etc/apache2/mods-enabled/mod-security.conf")).to contain 'Include "/etc/apache2/mod_security/*.conf"'
+  end
+
+  it "apache2 has a mod-security load file" do
     expect(file("/etc/apache2/mods-enabled/mod-security.load")).to be_file
+  end
+
+  it "mod_security is configured" do
+    expect(file("/etc/apache2/mod_security/modsecurity.conf")).to be_file
+    expect(file("/etc/apache2/mod_security/modsecurity.conf")).to contain 'SecRuleEngine DetectionOnly'
   end
 
 end
